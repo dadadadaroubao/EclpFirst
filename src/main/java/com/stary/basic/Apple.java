63895af6580558bba64267e7c3311523fecd1121
dadadadaroubao <dadadaroubao@aliyun.com>
2020-06-16 16:09:17 +0800

    在分支上进行修复

--- src/main/java/com/stary/basic/Apple.java
+++ src/main/java/com/stary/basic/Apple.java
@@ -3,6 +3,8 @@ package com.stary.basic;
 public class Apple {
 	public static void main(String[] args) {
 		System.out.println("hello git");
+		System.out.println("Ctrl Shift 3");
+		System.out.println("我要分支");
 	}
 
 }
